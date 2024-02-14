3fb09652640db052415337932ac8822903857946
Stefan Zabrian <faniss2005@yahoo.com>
2024-02-14 20:57:40 +0200

    Initial Commit

---
+++ src/test/java/com/dev/voyagewell/VoyageWellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.voyagewell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageWellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
