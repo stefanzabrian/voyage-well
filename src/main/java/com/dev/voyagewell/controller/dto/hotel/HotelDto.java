ae38aa9215000ddded24159646794c66edcdefe4
Stefan Zabrian <faniss2005@yahoo.com>
2024-02-27 11:48:29 +0200

    Task 23

--- src/main/java/com/dev/voyagewell/controller/dto/hotel/HotelDto.java
+++ src/main/java/com/dev/voyagewell/controller/dto/hotel/HotelDto.java
@@ -1,5 +1,6 @@
 package com.dev.voyagewell.controller.dto.hotel;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,14 +20,24 @@ public class HotelDto {
     private String picture3;
     private String picture4;
     private String picture5;
+    @JsonProperty
     private boolean isFreeParking;
+    @JsonProperty
     private boolean isRestaurant;
+    @JsonProperty
     private boolean isBar;
+    @JsonProperty
     private boolean isSpa;
+    @JsonProperty
     private boolean isWifi;
+    @JsonProperty
     private boolean isAirConditioning;
+    @JsonProperty
     private boolean isRoomService;
+    @JsonProperty
     private boolean isBalcony;
+    @JsonProperty
     private boolean isTv;
+    @JsonProperty
     private boolean isRoomWifi;
 }
